8dca45a9425dceafe7c9f6ec118658e01866f8c1
Antoine-Riou <78356969+Antoine-Riou@users.noreply.github.com>
2021-05-03 23:26:32 +0200

    BIG UPDATE

--- script.js
+++ script.js
@@ -1,76 +1,14 @@
+// Back to top //
 
-function scrollFooter(scrollY, heightFooter)
-{
-    console.log(scrollY);
-    console.log(heightFooter);
-
-    if(scrollY >= ($('.footer').height()) + ($('.draft').height()) + ($('.content').height()) )
-    {
-        $('footer').css({
-            'bottom' : '0px'
-        });
-    }
-    else
-    {
-        $('footer').css({
-            'bottom' : '-' + heightFooter + 'px'
-        });
-    }
-}
-
-
-function scrollHeader(scrollY, heightFooter)
-{
-    console.log(scrollY);
-    console.log(heightFooter);
-
-    if(scrollY >= ($('header').height()) + 50)
-    {
-       $('.header').css({
-            'top' : '-' + ($('.header').height()) + 'px'
-        });
-    }
-    
-    else
-    {
-         $('.header').css({
-            'top' : '0px'
-        });
-    }
-}
-
-$(window).load(function(){
-    var windowHeight        = $(window).height(),
-        footerHeight        = $('footer').height(),
-        heightDocument      = (windowHeight) + ($('.content').height()) + ($('.draft').height()) + ($('footer').height()) - 20;
-
-    
-    
-    $('header').css({
-        'height' : windowHeight + 'px',
-        'line-height' : windowHeight + 'px'
-    });
-
-    $('.wrapper-parallax').css({
-        'margin-top' : windowHeight + 'px'
-    });
-
-    scrollFooter(window.scrollY, footerHeight);
-    scrollHeader(window.scrollY, footerHeight);
-
-    
-    window.onscroll = function(){
-        var scroll = window.scrollY;
-
-      
-        
-        $('header').css({
-            'background-position-y' : 50 - (scroll * 100 / heightDocument) + '%'
-        });
-
-        scrollFooter(scroll, footerHeight);
-        scrollHeader(scroll, footerHeight);
-    }
-});
-
+(function() {
+  $(document).ready(function() {
+    return $(window).scroll(function() {
+      return $(window).scrollTop() > 150 ? $("#back-to-top").addClass("show") : $("#back-to-top").removeClass("show")
+    }), $("#back-to-top").click(function() {
+      return $("html,body").animate({
+        scrollTop: "0"
+      })
+    })
+  })
+}).call(this);
 
