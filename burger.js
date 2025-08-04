26b3554320431ccac2edddf7927d5aad4c9b6752
Antoine-Riou <78356969+Antoine-Riou@users.noreply.github.com>
2025-08-05 00:09:59 +0200

    e

--- burger.js
+++ burger.js
@@ -5,10 +5,5 @@ const navLinks = document.getElementById('navLinks');
 
 burgerMenu.addEventListener('click', () => {
     burgerMenu.classList.toggle('cross');
-    
-    if (navLinks.style.height === '0px' || !navLinks.style.height) {
-        navLinks.style.height = navLinks.scrollHeight + 'px'; 
-    } else {
-        navLinks.style.height = '0px'; 
-    }
+    navLinks.classList.toggle('show');
 });
